ec1e43d1a4013e771725133e5dbeb0cda96c577a
rick.maniquiz <rafaelnico.maniquiz@gcash.com>
2025-06-30 15:38:11 +0800

    Final Submission Lab 1: Celcius to Fahrenheit Converter

---
+++ README.md
@@ -0,0 +1,6 @@
+# LAB 1: Celcius to Farenheit Converter
+
+### Cadet Name: Rafael Nico T. Maniquiz
+### Project Name: FECP-Java-Session1-Operators
+### Output Screenshot: 
+<img src="https://github.com/rick-maniquiz/FECP-Java-Session1-Operators/blob/6f7d1a8cf68ab56d5b4b420df9cf2e5e23e8fe97/screenhot.png"/>
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,16 @@ package org.example;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static float celciusToFahrenheit(float celcius){
+        return (celcius * 9)/5 + 32;
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        float celcius = scanner.nextFloat();
+        float fahrenheit = celciusToFahrenheit(celcius);
+        System.out.print("Temperature in Fahrenheit: " + String.format("%.2f", fahrenheit));
 
     }
 }
